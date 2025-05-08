06e67e2b908caf8b3dc23761d40306bbc13b2ff5
samyduraih <110995724+samyduraih@users.noreply.github.com>
2025-05-08 23:37:55 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "this" {
   ami           = each.value.ami_id
   instance_type = each.value.instance_type
   subnet_id     = each.value.subnet_id
+  key_name      = var.key_name
 
   tags = merge(
     {
@@ -11,14 +12,12 @@ resource "aws_instance" "this" {
     each.value.tags
   )
 
-user_data = <<-EOF
+  user_data = <<-EOF
               #!/bin/bash
               hostnamectl set-hostname ${each.key}
               echo "${each.key}" > /etc/hostname
               sed -i "/127.0.1.1/d" /etc/hosts
               echo "127.0.1.1 ${each.key}" >> /etc/hosts
               echo "preserve_hostname: true" >> /etc/cloud/cloud.cfg
-            EOF
-
+              EOF
 }
-
